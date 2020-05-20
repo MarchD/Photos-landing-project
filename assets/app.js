0fe99a98b1e009d23560e1d080a213aed7794e2b
mdubchak <dubchmar@gmail.com>
2020-05-20 22:27:29 +0300

    add-js

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+
+let navtoggle = document.querySelector('.nav-toggle');
+let nav = document.querySelector('.nav');
+
+let toggleMenu = function () {
+        nav.classList.toggle("nav-open");
+    }
+
+navtoggle.addEventListener('click', function(e) {
+    e.stopPropagation();
+    toggleMenu();
+})
+
+document.addEventListener('click', function(e) {
+    let target = e.target;
+    let its_nav = target == nav || nav.contains(target);
+    let its_navtoggle = target == navtoggle;
+    let nav_is_active = nav.classList.contains('nav-open');
+
+    if (!its_nav && !its_navtoggle && nav_is_active) {
+        toggleMenu();
+    }
+});
+
