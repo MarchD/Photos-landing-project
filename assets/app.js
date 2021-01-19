ab060534b204a10df69b13dfce942fbdae3c9287
MarchD <dubchmar@gmail.com>
2021-01-19 20:24:55 +0200

    improved classes

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,21 @@
-
 let navtoggle = document.querySelector('.nav-toggle');
 let nav = document.querySelector('.nav');
 
 let toggleMenu = function () {
-        nav.classList.toggle("nav-open");
-    }
+    nav.classList.toggle("nav-open");
+}
 
-navtoggle.addEventListener('click', function(e) {
+navtoggle.addEventListener('click', function (e) {
     e.stopPropagation();
     toggleMenu();
 })
 
-document.addEventListener('click', function(e) {
-    let target = e.target;
+document.addEventListener('click', function (e) {
+    const target = e.target;
     let its_nav = target == nav || nav.contains(target);
     let its_navtoggle = target == navtoggle;
     let nav_is_active = nav.classList.contains('nav-open');
-
+    
     if (!its_nav && !its_navtoggle && nav_is_active) {
         toggleMenu();
     }
